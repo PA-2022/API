c84fe0c14de3505421c9c1c428b078c28f445ebc
Romain Kamiri <rckamiri@gmail.com>
2022-04-02 00:02:42 +0200

    added API

---
+++ codeup/src/main/java/pa/codeup/codeup/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package pa.codeup.codeup.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import pa.codeup.codeup.entities.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	@Query("SELECT u FROM User u WHERE u.email = ?1")
+	public User findByEmail(String email);
+	
+}
---
+++ codeup/src/main/java/pa/codeup/codeup/services/CustomUserDetailsService.java
@@ -0,0 +1,25 @@
+package pa.codeup.codeup.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import pa.codeup.codeup.entities.CustomUserDetails;
+import pa.codeup.codeup.entities.User;
+import pa.codeup.codeup.repositories.UserRepository;
+
+public class CustomUserDetailsService implements UserDetailsService {
+
+	@Autowired
+	private UserRepository userRepo;
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		User user = userRepo.findByEmail(username);
+		if (user == null) {
+			throw new UsernameNotFoundException("User not found");
+		}
+		return new CustomUserDetails(user);
+	}
+
+}
