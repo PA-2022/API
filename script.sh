81d198af1112014bbcfb1a74dd5b69f6df212113
Halisia-lab <63605419+Halisia-lab@users.noreply.github.com>
2022-04-13 16:35:04 +0200

    Delete script.sh

--- script.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-cd /home/runner/.ssh/ && echo "${{ secrets.EC2_SSH_KEY }}" > rsa-key.pem && chmod 400 rsa-key.pem 
-sudo nohup ssh -o StrictHostKeyChecking=no -tt -i "/home/runner/.ssh/rsa-key.pem" ec2-user@${{ secrets.HOST_DNS }} &
-        
