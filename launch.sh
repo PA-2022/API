af4c7c2329833c28585e96acc6a92b106c16109c
Halisia Halifa <halifa.halisia@gmail.com>
2022-04-14 19:57:42 +0200

    test

--- launch.sh
+++ launch.sh
@@ -4,5 +4,5 @@ cd API
 git pull origin halisia
 cd codeup
 mvn clean install 
-cd target
-java -Dspring.profiles.active=dev -jar codeup-0.0.1-SNAPSHOT.jar 
+# cd target
+# java -Dspring.profiles.active=dev -jar codeup-0.0.1-SNAPSHOT.jar 
