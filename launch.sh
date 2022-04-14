8470652107d34faf4df50c151147e13294445996
Halisia Halifa <halifa.halisia@gmail.com>
2022-04-14 15:56:02 +0200

    changes

--- launch.sh
+++ launch.sh
@@ -3,6 +3,6 @@ cd PA
 cd API
 git pull origin halisia
 cd codeup
-mvn clean install &
+mvn clean install 
 cd target
-java -jar codeup-0.0.1-SNAPSHOT.jar 
+nohup java -jar codeup-0.0.1-SNAPSHOT.jar &>/dev/null &
