04cf1ef245d6c4edcb945dc6a8a9bb1e9f53d768
Halisia Halifa <halifa.halisia@gmail.com>
2022-04-17 19:04:40 +0200

    test CD

--- launch.sh
+++ launch.sh
@@ -6,8 +6,9 @@ cd codeup
 mvn clean install 
 cd target
 
-if [ $(ps aux | grep "jar codeup-0.0.1-" | head -1 | cut -d' ' -f2) != null]
+if [ $(ps aux | grep "jar codeup-0.0.1-" | head -1 | cut -d' ' -f2) != null ]
 then
-kill $(ps aux | grep "jar codeup-0.0.1-" | head -1 | cut -d' ' -f2)
+    kill $(ps aux | grep "jar codeup-0.0.1-" | head -1 | cut -d' ' -f2)
+fi
 
-java -Dspring.profiles.active=dev -jar codeup-0.0.1-SNAPSHOT.jar 
+java -Dspring.profiles.active=dev -jar codeup-0.0.1-SNAPSHOT.jar
