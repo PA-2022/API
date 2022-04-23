8872838cdd07fde4efa11ee6796960d7235f387b
Halisia-lab <63605419+Halisia-lab@users.noreply.github.com>
2022-04-23 17:11:20 +0200

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 cd PA
 cd API
-git pull origin halisia
+git pull origin romain
 cd codeup
 mvn clean install 
 cd target
-kill $(ps aux | grep "jar codeup-0.0.1-" | head -1 | cut -d' ' -f2)
-java -Dspring.profiles.active=dev -jar codeup-0.0.1-SNAPSHOT.jar
+kill $(ps aux | grep "jar codeup-0.0.1-" | grep -v "grep" | head -1 | cut -d' ' -f2)
+nohup java -Dspring.profiles.active=dev -jar codeup-0.0.1-SNAPSHOT.jar & 
