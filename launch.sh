e9ea779000dc8be8634699308ebd405e098db592
Halisia Halifa <halifa.halisia@gmail.com>
2022-04-23 18:54:43 +0200

    test CD

--- launch.sh
+++ launch.sh
@@ -6,4 +6,4 @@ cd codeup
 mvn clean install 
 cd target
 kill $(ps aux | grep "jar codeup-0.0.1-" | grep -v "grep" | head -1 | awk '{print $2}')
-java -Dspring.profiles.active=dev -jar codeup-0.0.1-SNAPSHOT.jar &
+java -Dspring.profiles.active=dev -jar codeup-0.0.1-SNAPSHOT.jar 
