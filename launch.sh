07a8d50daf945e7e12a836215c5a049a44ab2de7
Halisia-lab <63605419+Halisia-lab@users.noreply.github.com>
2022-04-23 18:00:37 +0200

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 cd PA
 cd API
-git pull 
-#TODO uncomment the followings lines when java project is available in this branch 
-# cd codeup
-# mvn clean install 
-# cd target
-# kill $(ps aux | grep "jar codeup-0.0.1-" | head -1 | cut -d' ' -f2)
-# java -Dspring.profiles.active=dev -jar codeup-0.0.1-SNAPSHOT.jar
+git pull origin halisia
+cd codeup
+mvn clean install 
+cd target
+kill $(ps aux | grep "jar codeup-0.0.1-" | grep -v "grep" | head -1 | awk '{print $2}')
+nohup java -Dspring.profiles.active=dev -jar codeup-0.0.1-SNAPSHOT.jar &
