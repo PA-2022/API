a059ce4198e10cbaf291b077fef045ecfc05b60d
Halisia-lab <63605419+Halisia-lab@users.noreply.github.com>
2022-04-23 17:26:07 +0200

    Create launch.sh

---
+++ launch.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+cd PA
+cd API
+git checkout halisia
+git pull origin halisia
+cd codeup
+mvn clean install 
+cd target
+kill $(ps aux | grep "jar codeup-0.0.1-" | grep -v "grep" | head -1 | cut -d' ' -f2)
+nohup java -Dspring.profiles.active=dev -jar codeup-0.0.1-SNAPSHOT.jar & 
