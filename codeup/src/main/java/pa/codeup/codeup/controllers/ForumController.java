13141d3b190b98d1d530ba206b7c68c948d8fea5
rckam <rckmiri@gmail.com>
2022-05-02 10:45:48 +0200

    updates within forum

--- codeup/src/main/java/pa/codeup/codeup/controllers/ForumController.java
+++ codeup/src/main/java/pa/codeup/codeup/controllers/ForumController.java
@@ -1,10 +1,14 @@
 package pa.codeup.codeup.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import pa.codeup.codeup.entities.Forum;
 import pa.codeup.codeup.repositories.ForumRepository;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("forums")
@@ -22,5 +26,11 @@ public class ForumController {
     @ResponseBody
     public Long createForum(@RequestBody Forum forum){
         return forumRepository.save(forum).getId();
+    }
+
+    @GetMapping("/all/limit/{limit}/offset/{offset}")
+    @ResponseBody
+    public List<Forum> getAllWithLimit(@PathVariable int limit, @PathVariable int offset){
+        return forumRepository.findBy(PageRequest.of(offset, limit));
     }
 }
--- codeup/src/main/java/pa/codeup/codeup/repositories/ForumRepository.java
+++ codeup/src/main/java/pa/codeup/codeup/repositories/ForumRepository.java
@@ -1,12 +1,13 @@
 package pa.codeup.codeup.repositories;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import pa.codeup.codeup.entities.Forum;
-import pa.codeup.codeup.entities.User;
 
 import java.util.List;
 
 public interface ForumRepository extends JpaRepository<Forum, Long> {
 	public Forum getForumById(Long id);
+	public List<Forum> findBy(Pageable pageable);
 
 }
