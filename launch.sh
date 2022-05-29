dcbed6465dc4b989df016e9fce8c4b05bdb3d7ad
Halisia-lab <63605419+Halisia-lab@users.noreply.github.com>
2022-05-29 19:42:02 +0200

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -7,5 +7,5 @@ cd codeup
 mvn clean install 
 cd target
 kill $(ps aux | grep "jar codeup-0.0.1-" | grep -v "grep" | head -1 | awk '{print $2}')
-nohup java -Dspring.profiles.active=dev -jar codeup-0.0.1-SNAPSHOT.jar 
+nohup java -Dspring.profiles.active=dev -jar codeup-0.0.1-SNAPSHOT.jar &
 
