3e2c13b0774d04243cdaea2a113c3a3b1e995da5
Halifa Halisia <halifahalisia@MacBook-Pro-de-sanofi.local>
2022-07-20 00:28:36 +0200

    clean

--- codeup/src/main/java/pa/codeup/codeup/repositories/PostVoteRepository.java
+++ codeup/src/main/java/pa/codeup/codeup/repositories/PostVoteRepository.java
@@ -5,10 +5,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import pa.codeup.codeup.dto.PostVote;
 
 import java.util.Optional;
-import java.util.List;
 
 public interface PostVoteRepository extends JpaRepository<PostVote, Long> {
-	public Optional<List<PostVote>> findPostVoteByPostIdAndUserId(Long commentId, Long userId);
+	public Optional<PostVote> findPostVoteByPostIdAndUserId(Long commentId, Long userId);
 
     public int countAllByPostIdAndUpvote(Long id, boolean isUpvote);
 }
