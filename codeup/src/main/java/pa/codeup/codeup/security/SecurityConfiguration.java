12f0c7c885458034f7762e22ac082758a329917f
Halisia Halifa <halifa.halisia@gmail.com>
2022-07-26 21:13:07 +0200

    test ec2

--- codeup/src/main/java/pa/codeup/codeup/security/SecurityConfiguration.java
+++ codeup/src/main/java/pa/codeup/codeup/security/SecurityConfiguration.java
@@ -71,7 +71,7 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                 HttpMethod.DELETE.name()
         ));
         configuration.setAllowCredentials(true);
-        String[] origins = {"http://codeup-pa.herokuapp.com"};
+        String[] origins = {"http://codeup-pa.herokuapp.com", "http://localhost:4200"};
         configuration.setAllowedOrigins(new ArrayList<>(Arrays.asList(origins)));
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         source.registerCorsConfiguration("/**", configuration.applyPermitDefaultValues());
